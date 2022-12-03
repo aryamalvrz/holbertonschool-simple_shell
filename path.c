0f6de862856ea64c6c130a3d04bb5e5f1e9862cf
aryamalvrz <aryamalvrz@gmail.com>
2022-12-03 04:05:26 +0000

    On this commit I will add the function path

---
+++ path.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * path - Function that gets the path from environment
+ *
+ * @env: Environment variables
+ *
+ * Return: A string that contains enviorments, or NULL if fail
+ */
+
+char **path(char **env)
+{
+	int i = 0;
+	char *path, **copy_path, **path_tok;
+
+	while (env[i])
+	{
+		if (_strncmp(env[i], "PATH", 4) == 0)
+		{
+			path = _strdup(env[i]);
+			copy_path = tokenization(path, "=");
+			free(path);
+			path = _strdup(copy_path[1]);
+			free_array(copy_path);
+		}
+		i++;
+	}
+	path_tok = tokenization(path, ":");
+	return (path_tok);
+}
