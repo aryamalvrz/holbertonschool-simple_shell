67d590754e73248e6887528815d70e71d77dbe3a
aryamalvrz <aryamalvrz@gmail.com>
2022-12-03 16:47:05 +0000

    On this commit I will add the token file without the extra lines on the comments

--- token.c
+++ token.c
@@ -7,13 +7,6 @@
  * @delim: Delimiter
  *
  * Return: Array of tokens
- *
- * Description: We first allocate space for the array of tokens. We extract
- * our fits token and save it in token. We start a loop that will run while
- * token have something. Inside we allocate space for the posision inside
- * the index. Then we copy the string from token into the index. We clean
- * out token variable and start the process again. After our while our last
- * index is assign as NULL, to mark the end of the array.
  */
 
 char **tokenization(char *ptr, char *delim)
